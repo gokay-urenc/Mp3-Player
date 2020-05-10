cd13066f0ca8410f6649a27b51a2d32d94482ee6
Gökay <48670495+gokay-urenc@users.noreply.github.com>
2020-05-10 03:33:35 +0300

    Initial commit

---
+++ MyMp3Player/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MyMp3Player
+{
+    public partial class Form1 : Form
+    {
+        private Mp3Player mp3Player = new Mp3Player();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "MP3 Files|*.mp3|MP4 Files|*.mp4|WAV Files|*.wav";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    mp3Player.open(ofd.FileName);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mp3Player.play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mp3Player.stop();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Mp3-Player
+ Mp3 oynatıcı. / Mp3 player.
